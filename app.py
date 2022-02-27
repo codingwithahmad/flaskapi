dafd325d03139cf2e2da7a9fb98ae666da6da03c
codingwithahmad <amirahmad.seraji1996@gmail.com>
2022-02-27 22:08:02 +0330

    add post_language and get_language view and change get view names to get_languages

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, jsonify
+from flask import Flask, request
 
 
 app = Flask(__name__)
 
 languages = ['Python', 'JavaScript', 'Java']
 
+@app.route('/', methods=['POST'])
+def post_language():
+    arg = request.get_json()
+    print(arg)
+    if arg.get('name'):
+        languages.append(arg['name'])
+        return {}, 201
+    return {}
+
 
 @app.route('/', methods=['GET'])
-def get():
+def get_languages():
     return { "languages": languages }
+
+
+@app.route('/<int:index>', methods=['GET'])
+def get_language(index):
+    try:
+        return {'language': languages[index]}
+    except IndexError as e:
+        return {}, 404
